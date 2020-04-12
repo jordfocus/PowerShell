64816ce4a5cd2f27e3d99dfc5cc788464735e4cf
jordan.atanasovski <Jordan.Atanasovski@deltatre.com>
2020-04-12 19:57:42 +0200

    Initial commit

---
+++ covid19-charts/charts-lib.ps1
@@ -0,0 +1,56 @@
+﻿[void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms.DataVisualization")
+
+function CreateChart($width, $height, $backgroundColor)
+{
+   $chart = New-object System.Windows.Forms.DataVisualization.Charting.Chart
+   $chart.Width = $width
+   $chart.Height = $height
+   $chart.BackColor = $backgroundColor
+
+   return $chart
+}
+
+function AddSerie($chart, $serieName, $datasource, $datasourceX, $datasourceY, $color, $chartType)
+{
+    if($chartType -eq $null)
+    {
+        $chartType = "Column"  
+    }
+
+    [void]$chart.Series.Add($serieName)
+   $chart.Series[$serieName].ChartType = $chartType #https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.datavisualization.charting.seriescharttype?view=netframework-4.8
+   $chart.Series[$serieName].BorderWidth  = 3
+   $chart.Series[$serieName].IsVisibleInLegend = $true
+   # $chart.Series[$serieName].chartarea = "ChartArea1"
+   # $chart.Series[$serieName].Legend = "Legend1"
+   $chart.Series[$serieName].color = $color
+   $datasource | ForEach-Object {$chart.Series[$serieName].Points.addxy( $_.$datasourceX , $_.$datasourceY) }   
+}
+
+function SetupChartTitle($chart, $title, $font, $alignment)
+{
+   [void]$chart.Titles.Add($title)
+   $chart.Titles[0].Font = $font
+   $chart.Titles[0].Alignment = $alignment
+}
+
+function SetupChartArea($chart, $name, $axisXTitle, $axisYTitle, $axisXInterval, $axisYInterval)
+{
+   $chartarea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea
+   $chartarea.Name = $name
+   $chartarea.AxisY.Title = $axisYTitle
+   $chartarea.AxisX.Title = $axisXTitle
+   $chartarea.AxisY.Interval = $axisYInterval
+   $chartarea.AxisX.Interval = $axisXInterval
+   $chart.ChartAreas.Add($chartarea)
+  
+}
+
+function SetupChartLegend($chart, $name)
+{
+   $legend = New-Object system.Windows.Forms.DataVisualization.Charting.Legend
+   $legend.name = $name
+   $chart.Legends.Add($legend)
+}
+
+
---
+++ covid19-charts/covid-chart.ps1
@@ -0,0 +1,69 @@
+﻿# Import common scripts
+. ".\charts-lib.ps1"
+
+# Set Script Variables
+$outputFolder = "C:\CovidCharts\";
+$countryCode = "MK";
+$daysToAnalize = 50;
+
+function CreateCovidChart($covidDataSource, $covidDataSourceYAxis, $YAxisName, $backgroundColor, $chartColor, $chartType)
+{
+    if($backgroundColor -eq $null)
+    {
+        $backgroundColor = "#FFFFFF"  
+    }
+
+    if($chartColor -eq $null)
+    {
+        $chartColor = "#62B5CC"  
+    }
+
+    # create chart object
+   $chart = CreateChart -width 2000 -height 1200 -backgroundColor "#FFFFFF"
+   # set title
+    SetupChartTitle -chart $chart -title ("Covid " + $YAxisName) -font "Arial,14pt" -alignment "topLeft"
+   # set chart area 
+    $axisYIntervalStep = $null #$covidDataSource | Select-Object $covidDataSourceYAxis | Measure-Object -Maximum
+    SetupChartArea -chart $chart -name "CovidChartArea" -axisYTitle $YAxisName -axisXTitle "Date"  -axisXInterval 1 #-axisYInterval 100
+   # set legend 
+    SetupChartLegend -chart $chart -name "Legend"
+   #set serie 
+     $datasource = $covidDataSource | Select-Object date, $covidDataSourceYAxis
+     AddSerie -chart $chart -serieName $YAxisName -datasource $datasource -datasourceX "date" -datasourceY $covidDataSourceYAxis -color "#62B5CC" -chartType $chartType
+   # save the chart
+     
+     $chart.SaveImage($outputFolder+$countryCode + "-" + $YAxisName+".png","png")
+}
+
+
+$j = Invoke-WebRequest ('https://api.thevirustracker.com/free-api?countryTimeline=' + $countryCode) | ConvertFrom-Json 
+
+$startDate = (Get-Date).AddDays(-$daysToAnalize)
+$currentDate = Get-Date
+$covidDataArray = @();
+
+# transform API data
+for($i = $startDate; $i -lt $currentDate; $i = $i.AddDays(1))
+{
+    $date = $i.ToString("M/dd/yy")
+
+    foreach($covid19Data in $j.timelineitems) {
+        $tmp = $covid19Data.$date;
+        if($tmp)
+        {
+            $tmp | Add-Member NoteProperty "date" $date
+        }
+        $covidDataArray += $tmp;
+    }
+}
+
+#create charts
+CreateCovidChart -covidDataSource $covidDataArray -covidDataSourceYAxis "new_daily_cases" -YAxisName "Covid Daily Cases" -chartType "Column"
+CreateCovidChart -covidDataSource $covidDataArray -covidDataSourceYAxis "total_cases" -YAxisName "Total Cases" -chartType "Column"
+CreateCovidChart -covidDataSource $covidDataArray -covidDataSourceYAxis "new_daily_deaths" -YAxisName "Daily Deaths" -chartType "Line"
+
+#create CSV
+$covidDataArray | Select-Object | Export-Csv ($outputFolder+$countryCode + "-covid19.csv")
+
+#powershell.exe -executionpolicy bypass -file C:\Users\user\Documents\PowerShell\covid-chart.ps1
+
